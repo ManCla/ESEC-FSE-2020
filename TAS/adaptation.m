ec297358ff201507ab9c3ca7f420f4d598b83dac
Claudio Mandrioli <claudio@neumann.control.lth.se>
2020-05-25 19:39:44 +0200

    removed commented code

--- TAS/adaptation.m
+++ TAS/adaptation.m
@@ -3,23 +3,19 @@
 
 function new_controller_state=adaptation(success ,fail ,controller_state, gain, advanced)
 
+%% if advanced controller enabled reset the weight to one
 if advanced
     for iii=1:length(success)     
         if success(iii)==0 && fail(iii)>0 %attempt to recognize very weak providers and down providers
             new_controller_state(iii)=1;
-    %    elseif success(iii)==0 && fail(iii)==0 % to not completely forget about providers
-    %        new_controller_state(iii)=controller_state(iii);
         else
             new_controller_state(iii)=controller_state(iii)+gain*success(iii)-gain*fail(iii);
         end
     end    
 end
 
-%%    Carota e bastone with gain ±1 and some saturation
+%% control action computation
 new_controller_state=controller_state+gain*success-gain*fail;
-
-%% uniform control strategy (equivalent to setting gain=0)
-%new_controller_state = controller_state;
 
 %% saturation
 new_controller_state=min(max(new_controller_state,1),1000); 
