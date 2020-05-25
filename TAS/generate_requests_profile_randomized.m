90ee40771d9d4836721ff1d8c00e6527dbb83fb7
Claudio Mandrioli <claudio@neumann.control.lth.se>
2020-05-25 18:19:12 +0200

    changed internal names and commented request profile generation function

--- TAS/generate_requests_profile_randomized.m
+++ TAS/generate_requests_profile_randomized.m
@@ -1,17 +1,18 @@
-%function to generate a request profile
+%function to generate a partially randomized request profile.
+% the idea is to have an underlying deterministic trend to ensure
+% appropriate triggering of the adaptation strategy added to a randomized
+% component that makes the tests different form one another
 
-function requests_profile=generate_requests_profile_randmized()
+function requests_profile=generate_requests_profile_randomized()
 
+one=40*ones(50,1)+floor((rand(50,1)-0.5)*30); %constant request arrival rate
+two=linspace(50,200,11)'+floor((rand(11,1)-0.5)*40); %ramp input
+three=3*ones(39,1)+floor((rand(39,1))*70);
 
-primi_cinquanta=40*ones(50,1)+floor((rand(50,1)-0.5)*30); %constant request arrival rate
-secondi_undici=linspace(50,200,11)'+floor((rand(11,1)-0.5)*40); %ramp input
-terzi_trentanove=3*ones(39,1)+floor((rand(39,1))*70);
+requests_profile=[one; two; three];
 
-requests_profile=[primi_cinquanta;
-                  secondi_undici;
-                  terzi_trentanove;
-                  ];
-
-%requests_profile=randi([1,10],400,1);
+%add zeros requests at the end for avoiding that requests are still in
+%processing at the end of the simulation (needed for comparison consistency 
+%between the different tests)
 requests_profile=[requests_profile;zeros(50,1)];
 end
