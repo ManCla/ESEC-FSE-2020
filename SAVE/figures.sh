24c5365024f1fb7c16d0abf9e82df37a94e5de82
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2020-06-02 13:02:54 +0200

    figure 4 generation working but buggy

--- SAVE/figures.sh
+++ SAVE/figures.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-if $1=
+if [ "$1" = "precomputed" ]; then
+	DIR_RESULTS=final_results
+else
+	DIR_RESULTS=results/summary
+fi
+
+mkdir fig/data
+
+cat $DIR_RESULTS/greedy/*.csv > fig/data/all_greedy.csv
+cat $DIR_RESULTS/random/*.csv > fig/data/all_random.csv
+cat $DIR_RESULTS/integral/*.csv > fig/data/all_integral.csv
+cat $DIR_RESULTS/mpc/*.csv > fig/data/all_mpc.csv
+
+cd fig
+
+pdflatex Fig4.tex
+rm *.log *.aux
+
+cd ..
+
+rm -r fig/data
+
